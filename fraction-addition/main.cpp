493a3a4b3eae8115212aebb3deaf1273bd5d1a28
twbf <twbueler@bvsd.org>
2016-09-11 20:28:49 -0800

    commenting

--- fraction-addition/main.cpp
+++ fraction-addition/main.cpp
@@ -1,4 +1,5 @@
-/*Thomas Bueler
+/* Thomas Bueler   Addition of 2 Fractions
+    I am using the linux terminal to run programs rather than an IDE. I think the terminal is simplier. Back to the program, it addes 2 fractions by muntipling the denominaters and then adding the numerator. It outpust the unreduced fraction. Then the fraction goes into the reduceFrac function. This function tests all inegers starting at 2 and going up to the current square root to the smallest numerator or denominator. It then cout's the reduced fraction.
 */
 #include <iostream>
 #include <math.h>
@@ -41,6 +42,7 @@ int reduceFrac(int num, int den){
         while (num % factor == 0 && den % factor == 0) {
             num = num/factor;
             den = den/factor;
+            sml = sml/factor;
         }
         factor++;
     }
@@ -53,3 +55,19 @@ int reduceFrac(int num, int den){
 
     return 0;
 }
+
+/*
+tv@bueler-gazelle:~/cs-class/fraction-addition$ ./main1
+
+Insert a fraction in the form a 'enter' b
+8
+36
+
+Insert another fraction in the form c 'enter' d
+169
+21
+
+Unreduced: 6252/756
+
+Reduced: 521/63
+*/
