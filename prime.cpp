5aacc771baf4b9ce56dc124cdbdb9920313affda
Thomas Bueler <twbueler@gmail.com>
2016-09-13 08:58:48 -0800

    Create prime.cpp

---
+++ prime.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, factor = 2, bigFactor=1;
+    cout << "Prime Numbers To:" << endl;
+    cin >> a;
+    for
+    while (factor <= sml){
+        while (a % factor == 0){
+            a = a/factor;
+            sml = sml/factor;
+            bigFactor = bigFactor*factor;
+        }
+        factor++;
+    }
+}
