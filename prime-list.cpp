e2df507c809cd56e794f94907787d3a20792cd89
Thomas Bueler <twbueler@gmail.com>
2016-09-16 07:57:19 -0800

    coping the history of prime-number.cpp

---
+++ prime-list.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Prime Numbers To:" << endl;
+    cin >> a;
+    for (int i = 1; i <= a; i++) {
+        int prime = 1, factor = 2;
+        while (factor < i){
+            if (i % factor == 0){
+                prime = 0;
+            }
+            factor++;
+        }
+        if (prime == 1){
+            cout << i << endl;
+        }
+    }
+}
+Contact GitHub API Training Shop Blog About
+© 2016 GitHub, Inc. Terms Priva
