4e83711859d5563dc759eb0f6b57cc15bf65c5c9
Thomas Bueler <twbueler@gmail.com>
2016-09-16 08:39:19 -0800

    Update prime-list.cpp

--- prime-list.cpp
+++ prime-list.cpp
@@ -1,25 +1,36 @@
 #include <iostream>
 #include <math.h>
 
+int 
+
 using namespace std;
 
 int main()
 {
-    int a;
+    int a, primes = 0, composite = 1;
     cout << "Prime Numbers To:" << endl;
     cin >> a;
-    for (int i = 1; i <= a; i++) {
-        int prime = 1, factor = 2;
-        while (factor < i){
+    cout << "\nThe prime Numbers Are:" << endl;
+    for (int i = 2; i <= a; i++) {
+        int prime = 1, factor = 2, b=i;
+        while (factor <= sqrt(b)){
             if (i % factor == 0){
                 prime = 0;
+                b=1;
             }
             factor++;
         }
         if (prime == 1){
-            cout << i << endl;
+            primes++;
+            cout << i << "\t";
+        }
+        else{
+            composite++;
         }
     }
+    cout << "\nNumber of Primes: " << primes << endl;
+    cout << "\nNumber of Composites: " << composite << endl;
 }
-Contact GitHub API Training Shop Blog About
-© 2016 GitHub, Inc. Terms Priva
+/*
+*/
+
