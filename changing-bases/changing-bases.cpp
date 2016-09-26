ab1ac6e652462f9edfbf0ee9c8f41b606c22d9cc
twbf <twbueler@bvsd.org>
2016-09-25 21:25:15 -0800

    changing bases

---
+++ changing-bases/changing-bases.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int userNumber, base;
+    std::cout << "Enter a number in base 10:" << std::endl;
+    std::cin >> userNumber;
+    std::cout << "What base do you want it in (2-10):" << std::endl;
+    std::cin >> base;
+}
