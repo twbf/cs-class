ae1936cdb2dd5de35fee428afafbd28ea721a1e4
Thomas Bueler <twbueler@gmail.com>
2016-10-06 08:49:39 -0800

    starting gift1 getting the files set up

---
+++ usaco-gift1/usaco-gift1.cpp
@@ -0,0 +1,20 @@
+/*
+ID: twbuele1
+LANG: C++
+PROG: gift1
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ofstream fout ("gift1.out");
+    ifstream fin ("gift1.in");
+    int numberPeople;
+    fin >> numberpeople;
+    return 0;
+}
