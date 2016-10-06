6af38e2e5a18e783db23e32219362963c442a22a
Thomas Bueler <twbueler@gmail.com>
2016-10-06 09:00:56 -0800

    hgfgh

--- usaco-gift1/usaco-gift1.cpp
+++ usaco-gift1/usaco-gift1.cpp
@@ -12,9 +12,15 @@ using namespace std;
 
 int main()
 {
-    ofstream fout ("gift1.out");
+    //ofstream fout ("gift1.out");
     ifstream fin ("gift1.in");
     int numberPeople;
-    fin >> numberpeople;
+    int people[10];
+    fin >> numberPeople;
+    while(i<=numberPeople){
+        fin >> people[i];
+        cout << people [i] << endl;
+        i++
+    }
     return 0;
 }
