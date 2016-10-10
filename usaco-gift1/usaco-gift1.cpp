3b4ef32ff6340cc00474ba4118dc5b249593c5e7
Thomas Bueler <twbueler@gmail.com>
2016-10-10 08:35:30 -0800

    debugging

--- usaco-gift1/usaco-gift1.cpp
+++ usaco-gift1/usaco-gift1.cpp
@@ -18,41 +18,50 @@ int main()
     ifstream fin ("gift1.in");
     int numberPeople, i = 0;
     char people[25][25];
+    int personId = 0;
     fin >> numberPeople;
-    cout << numberPeople;
     while(i<=numberPeople-1){
         fin >> people[i];
-        cout << people[i] << endl;
         i++;
     }
     i=0;
-    cout << numberPeople;
-    int money[10], moneyGived;
+    int money[10] = {}, moneyGived;
     char giver[25];
-    while(i<=numberPeople-1){
+    while(i<=numberPeople-2){
         //cout << numberPeople;
         int numGiftees = 0, j=0;
         fin >> giver;
         fin >> moneyGived;
         fin >> numGiftees;
-        money[i]-=moneyGived-(moneyGived%numGiftees);
+        for(int h=0; h<=20; h++){
+            if(giver == people[h]){
+                personId = h;
+            }
+        }
+        cout << personId;
+        money[personId]-=moneyGived-(moneyGived%numGiftees);
+        //cout << money[i] << endl;
         while(j<=numGiftees-1){
             char giftee[25];
             fin >> giftee;
-            int personId = 0;
             for(int h=0; h<=20; h++){
                 if(giftee == people[h]){
                     personId = h;
                 }
             }
             //cout << numberPeople;
-            money[personId] = numGiftees%moneyGived;
+            money[personId] += moneyGived/numGiftees;
+            //cout << people[i] << ' ' << money[i] << endl;
             //cout << personId << endl;
-            cout << money[personId] << endl;
-            cout << moneyGived << endl;
-            cout << numGiftees << endl;
+            //cout << money[personId] << endl;
+            //cout << moneyGived << endl;
+            //cout << numGiftees << endl;
             j++;
         }
+        i++;
+    }
+    for(int h=0; h<=numberPeople; h++){
+        cout << people[h] << ' ' << money[h] << endl;
     }
     return 0;
 }
