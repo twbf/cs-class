25cb61e6404b00992129fa74762a9ccba8aced87
twbf <twbueler@gmail.com>
2016-10-15 13:08:51 -0800

    about to hike

--- usaco-friday/main.cpp
+++ usaco-friday/main.cpp
@@ -12,18 +12,33 @@ using namespace std;
 
 int main(){
     ofstream fout ("friday.out");
-    ifstream sfin ("friday.in");
-    int daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    int day, date;
+    ifstream fin ("friday.in");
+    int daysInMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int day = 2;
     int count13[7] = {};
+    int exactDays;
     int endYear;
-    cin << endYear;
+    fin >> endYear;
     endYear += 1899;
     for (int i = 1900; i < endYear; i++){
         for (int j = 0; j<12; j++){
-            for (int h = 1; h <= daysInMonth[j]; h++){
+            exactDays = daysInMonth[j];
+            if ((i%4 == 0) && j==1){
+                exactDays = 29;
+            }
+            if ((i%100 == 0) && j==1){
+                exactDays = 28;
+            }
+            if ((i%400 == 0) && j==1){
+                exactDays = 29;
+            }
+            for (int h = 1; h <= exactDays; h++){
+                day++;
                 if (h==13){
-                    count13[day]++;
+                    count13[day] += 1;
+                }
+                if(day == 6){
+                    day = -1;
                 }
             }
         }
