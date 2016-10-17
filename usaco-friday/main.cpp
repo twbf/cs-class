55a8fb870f617eb3b6ec08f413c0f102c11641db
Thomas Bueler <twbueler@gmail.com>
2016-10-17 08:21:54 -0800

    done with friday. Tested

--- usaco-friday/main.cpp
+++ usaco-friday/main.cpp
@@ -15,11 +15,11 @@ int main(){
     ifstream fin ("friday.in");
     int daysInMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     int day = 2;
-    int count13[7] = {};
+    int count13[7] = {0,0,0,0,0,0,0};
     int exactDays;
     int endYear;
     fin >> endYear;
-    endYear += 1899;
+    endYear += 1900;
     for (int i = 1900; i < endYear; i++){
         for (int j = 0; j<12; j++){
             exactDays = daysInMonth[j];
@@ -32,19 +32,24 @@ int main(){
             if ((i%400 == 0) && j==1){
                 exactDays = 29;
             }
-            for (int h = 1; h <= exactDays; h++){
-                day++;
-                if (h==13){
+            for (int h = 1; h < exactDays + 1; h++){
+                if (h == 13){
                     count13[day] += 1;
                 }
-                if(day == 6){
-                    day = -1;
+                day++;
+                if(day == 7){
+                    day = 0;
                 }
             }
         }
     }
     for (int i=0; i<7; i++){
-        fout << count13[day] << ' ';
+        if (i<6){
+            fout << count13[i] << ' ';
+        }
+        else {
+            fout << count13[i] << endl;
+        }
     }
     return 0;
 }
