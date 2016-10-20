b69a51bf3d3319ef03f2c7bde0234cb63c533119
Thomas Bueler <twbueler@gmail.com>
2016-10-20 09:01:03 -0800

    dsfgufwj

--- MadLibs/main.cpp
+++ MadLibs/main.cpp
@@ -10,7 +10,7 @@ int main()
     char pName[30];
     char actionVerb[2][30];
     char adverb[30];
-    char nonPNouns[3][30];
+    char nonPNoun[3][30];
     char integer[30];
     char adj[2][30];
 
@@ -35,8 +35,24 @@ int main()
     cout << "Enter a non-proper noun:" << endl;
     cin >> nonPNoun[2];
 
+    cout << "Enter an integer:" << endl;
+    cin >> integer;
 
-    cout << "Once upon a time there was a computer science student named " << pName << " "
+    cout << "Enter an adjective:" << endl;
+    cin >> adj[0];
+
+    cout << "Enter an adjective:" << endl;
+    cin >> adj[1];
+
+
+    cout << "Once upon a time there was a computer science student named " << pName
+    << ". He/She built a " << adj[0] <<  " program that " << actionVerb[0] <<
+    ". This program changed how people lived their lives. It also changed " <<
+    integer << " " << nonPNoun[0] << "s for the better. The program made the " <<
+    nonPNoun[0] << "s " << adj[1] << " by always " << actionVerb[1] << ". " << pName
+    << " became insanely famous after his life changing invention. So famous that " <<
+    "he was hired at Apple as a " << nonPNoun[1] << " maker. He used a " << adverb << " "
+    << nonPNoun[2] << " to make a " << nonPNoun[1] << endl;
 
 
     return 0;
