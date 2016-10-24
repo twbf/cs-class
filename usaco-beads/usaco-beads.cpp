5a024c36b2af444290329b1d134c6dbac1bdedf2
Thomas Bueler <twbueler@gmail.com>
2016-10-23 20:11:37 -0800

    got program that finds the max number of w or bs in the string

--- usaco-beads/usaco-beads.cpp
+++ usaco-beads/usaco-beads.cpp
@@ -12,11 +12,27 @@ using namespace std;
 
 int main()
 {
-    ofstream fout ("test.out");
-    ifstream fin ("test.in");
-    int a, b;
-    fin >> a >> b;
-    fout << a+b << endl;
+    //ofstream fout ("test.out");
+    ifstream fin ("beads.in");
+    int numBeads;
+    char beads[370];
+    fin >> numBeads >> beads;
+    int maxBeads = 0;
+    int cmaxBeads = 1;
+    char prevChar[10] = {};
+    prevChar[0] = beads[0];
+    for(int i = 1; i < numBeads; i++){
+        if ((prevChar[0] == 'w' || prevChar[0] == 'b') && (beads[i] == 'w' || beads[i] == 'b')){
+            cmaxBeads++;
+            if (cmaxBeads > maxBeads){
+                maxBeads = cmaxBeads;
+            }
+        }
+        else {
+            cmaxBeads = 1;
+        }
+        prevChar[0] = beads[i];
+    }
+    cout << maxBeads << endl;
     return 0;
 }
-
