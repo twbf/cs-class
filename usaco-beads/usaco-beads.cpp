a99b2d225e8ce5e8939c5468af56f8f5f8298183
Thomas Bueler <twbueler@gmail.com>
2016-10-23 21:10:56 -0800

    testing max lengh

--- usaco-beads/usaco-beads.cpp
+++ usaco-beads/usaco-beads.cpp
@@ -17,22 +17,32 @@ int main()
     int numBeads;
     char beads[370];
     fin >> numBeads >> beads;
-    int maxBeads = 0;
-    int cmaxBeads = 1;
+    int maxBeads = 0, start;
+    int cmaxBeads = 0;
     char prevChar[10] = {};
     prevChar[0] = beads[0];
-    for(int i = 1; i < numBeads; i++){
-        if ((prevChar[0] == 'w' || prevChar[0] == 'b') && (beads[i] == 'w' || beads[i] == 'b')){
-            cmaxBeads++;
-            if (cmaxBeads > maxBeads){
-                maxBeads = cmaxBeads;
-            }
+    for (int j = 0; j<3; j++) {
+        if (j == 0){
+            start = 1;
         }
         else {
-            cmaxBeads = 1;
+            start = 0;
         }
-        prevChar[0] = beads[i];
+        for (int i = start; i < numBeads; i++){
+            if ((prevChar[0] == 'w' || prevChar[0] == 'b') && (beads[i] == 'w' || beads[i] == 'b')){
+                cmaxBeads++;
+                if (cmaxBeads > maxBeads){
+                    maxBeads = cmaxBeads;
+                }
+                cout << cmaxBeads << endl;
+            }
+            else {
+                cout << cmaxBeads << endl;
+                cmaxBeads = 0;
+            }
+            prevChar[0] = beads[i];
+        }
     }
-    cout << maxBeads << endl;
+    cout << maxBeads + 1 << endl;
     return 0;
 }
