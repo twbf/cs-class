da4f8a6373db9089ff69620128f4f131cc474a06
Thomas Bueler <twbueler@gmail.com>
2016-10-25 09:00:31 -0800

    Test

---
+++ usaco-milk2/usaco-milk2.cpp
@@ -0,0 +1,20 @@
+/*
+ID: twbuele1
+LANG: C++
+PROG: ride
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    //ofstream fout ("test.out");
+    ifstream fin ("milk.in");
+    int time[2][5000];
+    fin >> time;
+    return 0;
+}
