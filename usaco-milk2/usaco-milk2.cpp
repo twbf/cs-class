b17a7f28e39920f1e4cdde6d2ed5e08dc191e96e
Thomas Bueler <twbueler@gmail.com>
2016-10-27 09:00:27 -0800

    countinuing milking

--- usaco-milk2/usaco-milk2.cpp
+++ usaco-milk2/usaco-milk2.cpp
@@ -15,6 +15,26 @@ int main()
     //ofstream fout ("test.out");
     ifstream fin ("milk.in");
     int time[2][5000];
-    fin >> time;
+    int numFarmers;
+    fin >> numFarmers;
+    for (int i = 0; i < numFarmers; i++){
+        fin >> time[0][i];
+        fin >> time[1][i];
+    }
+    for (int i = 0; i < numFarmers; i++){
+        for (int j = 0; j < numFarmers; j++){
+            int tmp = 0;
+            if(time[0][j] > time [0][j+1]){
+                tmp = time[0][j];
+                time[0][j] = time[0][j+1];
+                time[0][j+1] = tmp;
+            }
+        }
+    }
+    //for (int i = 0; i < numFarmers; i++){
+        //cout << time[0][i] << ' ';
+        //cout << time[1][i] << endl;
+    //}
     return 0;
 }
+
