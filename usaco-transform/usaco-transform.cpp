84e72c299dacceab70aeec990c1497ed64e20ebe
Thomas Bueler <twbueler@gmail.com>
2016-11-07 08:14:26 -0900

    starting transform. Uncommitted changes at home

---
+++ usaco-transform/usaco-transform.cpp
@@ -0,0 +1,19 @@
+/*
+ID: twbuele1
+LANG: C++
+PROG: transform
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ofstream fout ("transform.out");
+    ifstream fin ("transform.in");
+
+    return 0;
+}
