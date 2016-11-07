5739d585bbc347cf16b168beffb15f06b0bd8649
Thomas Bueler <twbueler@gmail.com>
2016-11-07 08:40:30 -0900

    gyauvtgb

--- usaco-transform/usaco-transform.cpp
+++ usaco-transform/usaco-transform.cpp
@@ -14,6 +14,19 @@ int main()
 {
     ofstream fout ("transform.out");
     ifstream fin ("transform.in");
-
+    int arraySize;
+    fin >> arraySize;
+    char startPattern[arraySize][arraySize];
+    char endPattern[arraySize][arraySize];
+    for (int i = 0; i<arraySize; i++){
+        fin >> startPattern[i];
+    }
+    for (int i = 0; i<arraySize; i++){
+        fin >> endPattern[i];
+    }
+    if (startPattern[0][0] == endPattern[0][0]){
+        cout << 6 << endl;
+        return 0;
+    }
     return 0;
 }
