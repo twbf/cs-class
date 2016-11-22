f9b8bdafc7e62dd7179be42a2166053f5110e30e
Thomas Bueler <twbueler@gmail.com>
2016-11-22 08:58:49 -0900

    working on hailstone

---
+++ hailstone/hailstone.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+main (){
+    int startingNum;
+    cout << "Enter an Integer:" << endl;
+    cin << startingNum;
+    while(startingNum != 1){
+        if (startingNum%2==0){
+            
+        }
+    }
+}
