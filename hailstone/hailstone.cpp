6b1710b5e4d69846d313f6c338e9bea55551af6b
Thomas Bueler <twbueler@gmail.com>
2016-11-22 09:31:20 -0900

    almost done with hailstone

--- hailstone/hailstone.cpp
+++ hailstone/hailstone.cpp
@@ -3,12 +3,23 @@
 using namespace std;
 
 main (){
-    int startingNum;
-    cout << "Enter an Integer:" << endl;
-    cin << startingNum;
+    long long int startingNum;
+    int count = 0;
+    do {
+        cout << "Enter an Integer:" << endl;
+        cin >> startingNum;
+    } while (startingNum>0);
+    cout << startingNum << endl;
     while(startingNum != 1){
+        count++;
         if (startingNum%2==0){
-            
+            startingNum = startingNum/2;
         }
+        else{
+            startingNum = startingNum*3+1;
+        }
+        cout << startingNum << endl;
     }
+    cout << "STUCK!!" << endl;
+    cout << count+1 << endl;
 }
