a4d31c09915e001e1334aafa11e6eb1ffc9226a6
Thomas Bueler <twbueler@gmail.com>
2016-12-08 09:00:36 -0900

    dasgkfsgh

---
+++ usaco-namenum/namenum.cpp
@@ -0,0 +1,19 @@
+/*
+ID: twbuele1
+LANG: C++
+PROG: ride
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ofstream fout ("namenum.out");
+    ifstream fin ("namenum.in");
+    
+    return 0;
+}
