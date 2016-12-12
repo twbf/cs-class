96cfa1fb9841f5dd2e0757c6afe36c5eaaa65de7
Thomas Bueler <twbueler@gmail.com>
2016-12-12 08:59:40 -0900

    asdifhk

--- usaco-namenum/namenum.cpp
+++ usaco-namenum/namenum.cpp
@@ -14,6 +14,11 @@ int main()
 {
     ofstream fout ("namenum.out");
     ifstream fin ("namenum.in");
-    
+    ifstream finDict ("dict.txt");
+    char keyboard[8][3][2] = {{"A","B","C"},{"D","E","F"},{"G","H","I"},{"J","K","L"},{"M","N","O"}, {"P","R","S"}, {"T","U","V"}, {"W","X","Y"}};
+    char dict[5000][5];
+    for (int i=0; i<5000; i++){
+        finDict >> dict[i];
+    }
     return 0;
 }
