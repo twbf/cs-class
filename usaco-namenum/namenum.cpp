e73cc58bc13c3754a316e67a419d4664c2a50f63
Thomas Bueler <twbueler@gmail.com>
2016-12-16 07:54:00 -0900

    generating sequences recursivly

--- usaco-namenum/namenum.cpp
+++ usaco-namenum/namenum.cpp
@@ -7,7 +7,7 @@ PROG: ride
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <stdio.h>
+#include <math.h>
 #include <string.h>
 
 using namespace std;
@@ -19,13 +19,13 @@ int main()
     ifstream finDict ("dict.txt");
     char keyboard[8][3][2] = {{"A","B","C"},{"D","E","F"},{"G","H","I"},{"J","K","L"},{"M","N","O"}, {"P","R","S"}, {"T","U","V"}, {"W","X","Y"}};
     char dict[5000][5];
+    char number[20];
     for (int i=0; i<5000; i++){
         finDict >> dict[i];
     }
-    char num[20];
-    fin >> num;
-    for (int i=0; i<strlen(num); i++){
-
-    }
+    fin >> number;
+    for (int i =0; i<pow(3,strlen(number)); i++)(
+        
+    )
     return 0;
 }
