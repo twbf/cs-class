b617bb64140775f8067e80537feb185aa46517c0
Thomas Bueler <twbueler@gmail.com>
2017-01-12 09:00:47 -0900

    adsjkfhksj

--- hanoi/towers.cpp
+++ hanoi/towers.cpp
@@ -2,15 +2,22 @@
 
 using namespace std;
 
-void switchPens();
+void switchPens(int, int , int, int);
+void move(int, int);
+
+int pins [3][10];
 
 int main(){
-
+    switchPens(3,0,1,2);
     return 0;
 }
 
-switchPens(){
-    if(something){
-        switchPens
+void switchPens(int numPins, int original, int tmp, int moved){
+    if(numPins > 1){
+
+        switchPens(numPins-1, original, tmp, moved);
     }
+}
+void move(int firstPin, int secoundPin){
+
 }
