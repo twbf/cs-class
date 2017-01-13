30f702c7beba4e78382c194f1ee2fdeb4100dad8
Thomas Bueler <twbueler@gmail.com>
2017-01-13 08:38:32 -0900

    dsljkfl

--- hanoi/towers.cpp
+++ hanoi/towers.cpp
@@ -4,20 +4,32 @@ using namespace std;
 
 void switchPens(int, int , int, int);
 void move(int, int);
+void placeRings(int);
+int firstPin(int);
 
-int pins [3][10];
+int pins[3][10] = {};
 
 int main(){
+    placeRings(3);
     switchPens(3,0,1,2);
     return 0;
 }
 
 void switchPens(int numPins, int original, int tmp, int moved){
     if(numPins > 1){
-
+        
         switchPens(numPins-1, original, tmp, moved);
     }
 }
-void move(int firstPin, int secoundPin){
 
+void move(int firstPin, int twoPin){
+    int tmp = pins[firstPin][firstPin(firstPin)];
+    pins[firstPin][firstPin(firstPin)] = 0;
+    pins[twoPin][firstPin(twoPin)+1]=tmp
+}
+
+void placeRings(int pins){
+    for (int i = 0; i<pins; i++){
+        pins[0][i] = 1;
+    }
 }
