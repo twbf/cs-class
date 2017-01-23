538b070a04871e5e0bf839a65444f040ed651dfc
Thomas Bueler <twbueler@gmail.com>
2017-01-22 21:38:19 -0900

    djhsk

--- hanoi/towers.cpp
+++ hanoi/towers.cpp
@@ -3,55 +3,23 @@
 
 using namespace std;
 
-void switchPens(int, int , int, int, int);
-void move(int, int);
-void placeRings(int);
-int firstPin(int);
+void switchPens(int, char, char, char);
 
-int pins[3][10] = {};
 
 int main(){
-    placeRings(3);
-    switchPens(3,0,1,2,0);
+    int numPins;
+    cin >> numPins;
+    switchPens(numPins, 'A', 'B', 'C');
     return 0;
 }
 
-void switchPens(int numPins, int original, int tmp, int moved, int numMoved){
-    for(int i = 0; i< numMoved+1; i++){
-        switchPens(numPins-1, original, tmp, moved, numMoved +1)
-        move (original, moved);
-        switchPens(numPins-1, original, tmp, moved, numMoved +1)
-        if (numMoved == 0){
-            move(original, moved);
-        } else if (numMoved == 1){
-            move(moved, tmp);
-            move (original, moved);
-            move(tmp, moved);
-        }
-        else {
-            move (moved, original);
-            move(moved, tmp);
-            move (original, tmp);
-            move (original, moved);
-            move(tmp, original);
-            move (tmp, moved);
-            move (original, moved);
-        }
+void switchPens(int numPins, char source, char aux, char dest){
+    if (numPins == 1){
+        cout << source << " to " << dest << endl;
     }
-    if(numPins > 1){
-
-        switchPens(numPins-1, original, tmp, moved, numMoved +1);
-    }
-}
-
-void move(int firstPin, int twoPin){
-    int tmp = pins[firstPin][firstPin(firstPin)];
-    pins[firstPin][firstPin(firstPin)] = 0;
-    pins[twoPin][firstPin(twoPin)+1]=tmp
-}
-
-void placeRings(int pins){
-    for (int i = 0; i<pins; i++){
-        pins[0][i] = i+1;
+    else {
+        switchPens(numPins-1, source, aux, dest);
+        switchPens(1, source, dest, aux);
+        switchPens(numPins-1, aux, dest, source);
     }
 }
