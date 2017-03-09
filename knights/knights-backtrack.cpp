454799c55340b54bc2dfdd5b17fcbdda52d7c67f
Thomas Bueler <twbueler@gmail.com>
2017-03-09 09:00:35 -0900

    ajkh

--- knights/knights-backtrack.cpp
+++ knights/knights-backtrack.cpp
@@ -7,12 +7,13 @@
 using namespace std;
 
 int move(int, int, int);
-int testMove(int, int);
+int testMove(int, int, int);
 void zeroPlaces();
 int printBoard();
+int printMoves();
+int numMoves =0;
 
 int places[8][8] = {};
-int accesible[8][8] = {};
 int moves [64][3] = {};
 int hor[8], ver[8];
 
@@ -34,11 +35,13 @@ int main (){
     ver[5] = 2;
     ver[6] = 2;
     ver[7] = 1;
-    for (int i = 0; i<1; i++){
-        for (int j = 0; j<1; j++){
+    for (int i = 5; i<6; i++){
+        for (int j = 0; j<2; j++){
             cout << move(i,j,0) << endl;
             //printBoard();
+            //printMoves();
             zeroPlaces();
+            cout << numMoves << endl;
         }
     }
     return 0;
@@ -47,40 +50,49 @@ int main (){
 int move(int x, int y, int i){
     moves[i][0] = x;
     moves[i][1] = y;
-    moves[i][2]++;
-    i++;
-    printBoard();
-    if (i == 64) {
+    printMoves();
+    places[x][y] = 1;
+    if (i == 63) {
         return i;
     }
-    places[x][y] = 1;
-    int number = testMove(x,y);
-    //cout << number << endl;
+    printBoard();
+    int number = testMove(x,y,i);
+    numMoves++;
     if (number == -1){
-        i = move(moves[i--][0], moves[i--][0], i--);
+        places[x][y] = 0;
+        moves[i][0] = 0;
+        moves[i][1] = 0;
+        i = move(moves[i--][0], moves[i--][1], i--);
+    } else {
+        int nX = x+hor[number];
+        int nY = y+ver[number];
+        i++;
+        i = move(nX, nY, i);
     }
-    int nX = x+hor[number];
-    int nY = y+ver[number];
-    i = move(nX, nY, i);
     return i;
 }
 
-int testMove(int X, int Y){
+int testMove(int X, int Y, int j){
     int min = 0;
     bool validspace = false;
-    int counter = 
+    int cMoves = moves[j][2];
+    int counter = 0;
     for (int i = 0; i < 8; i++){
         int x = X+hor[i];
         int y = Y+ver[i];
         if ((0<=x && x<8) && (0<=y  && y<8) && (places[x][y] != 1)){
-            validspace = true;
-            min = i;
-            i = 8;
+            if(counter == cMoves){
+                validspace = true;
+                min = i;
+                i = 8;
+            }
+            counter++;
         }
     }
     if (validspace == false){
         return -1;
     }else{
+        moves[j][2]++;
         return min;
     }
 }
@@ -98,6 +110,14 @@ void zeroPlaces(){
          for (int j = 0; j<8; j++){
              cout << places[i][j] << " ";
          }
+         cout << endl;
+     }
+     return 0;
+ }
+ int printMoves(){
+     cout << endl;
+     for (int i = 0; i<64; i++){
+         cout << moves[i][0] << " , " << moves[i][1];
          cout << endl;
      }
      return 0;
