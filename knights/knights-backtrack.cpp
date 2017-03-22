f210bba5d8f3b41e9df18be0f46f640950eef249
Thomas Bueler <twbueler@gmail.com>
2017-03-21 20:22:19 -0800

    sfhjk

--- knights/knights-backtrack.cpp
+++ knights/knights-backtrack.cpp
@@ -11,7 +11,7 @@ int testMove(int, int, int);
 void zeroPlaces();
 int printBoard();
 int printMoves();
-int numMoves =0;
+long int numMoves = 0;
 
 int places[8][8] = {};
 int moves [64][3] = {};
@@ -35,7 +35,7 @@ int main (){
     ver[5] = 2;
     ver[6] = 2;
     ver[7] = 1;
-    for (int i = 5; i<6; i++){
+    for (int i = 0; i<1; i++){
         for (int j = 0; j<1; j++){
             cout << move(i,j,0) << endl;
             printBoard();
@@ -49,35 +49,41 @@ int main (){
 
 int move(int x, int y, int i){
     //printMoves();
-    printBoard();
-    moves[i][0] = x;
-    moves[i][1] = y;
-    cout << i << endl;
-    places[x][y] = 1;
-    if (i == 63) {
-        return i;
-    }
-    int number = testMove(x,y,i);
+    //printBoard();
     numMoves++;
-    if (number == -1){
-        moves[i][2]++;
-        if (moves[i][2] > 7){
-            places[x][y] = 0;
-            moves[i][0] = 0;
-            moves[i][1] = 0;
-            moves[i][2] = 0;
-            i--;
-            i--;
-            i = move(moves[i][0], moves[i][1], i);
-        } else {
-            i = move(moves[i][0], moves[i][1], i);
+    // if (moves[i][2] > 7){
+    //     cout << i << "fdk" << endl;
+    //     places[x][y] = 0;
+    //     moves[i][0] = 0;
+    //     moves[i][1] = 0;
+    //     moves[i][2] = 0;
+    //     i--;
+    //     moves[i][2]++;
+    //     i = move(moves[i][0], moves[i][1], i);
+    // } else {
+        moves[i][0] = x;
+        moves[i][1] = y;
+        cout << i << " "<< numMoves << endl;
+        places[x][y] = 1;
+        if (i == 62) {
+            return i;
         }
-    } else {
-        int nX = x+hor[number];
-        int nY = y+ver[number];
-        i++;
-        i = move(nX, nY, i);
-    }
+        int number = testMove(x,y,i);
+        if (number == -1){
+                places[x][y] = 0;
+                moves[i][0] = 0;
+                moves[i][1] = 0;
+                moves[i][2] = 0;
+                i--;
+                moves[i][2]++;
+                i = move(moves[i][0], moves[i][1], i);
+        } else {
+            int nX = x+hor[number];
+            int nY = y+ver[number];
+            i++;
+            i = move(nX, nY, i);
+        }
+    //}
     return i;
 }
 
@@ -89,13 +95,15 @@ int testMove(int X, int Y, int j){
     for (int i = 0; i < 8; i++){
         int x = X+hor[i];
         int y = Y+ver[i];
-        if ((0<=x && x<8) && (0<=y  && y<8) && (places[x][y] < 1)){
-            if(counter == cMoves){
-                validspace = true;
-                min = i;
-                i = 8;
+        if ((0<=x && x<8) && (0<=y  && y<8)){
+            if (places[x][y] < 1){
+                if(counter == cMoves){
+                    validspace = true;
+                    min = i;
+                    i = 8;
+                }
+                counter++;
             }
-            counter++;
         }
     }
     if (validspace == false){
