af62ed77634d051c70bec0abc7f3f6603463b325
twbf <twbueler@gmail.com>
2018-01-06 19:58:22 -0900

    only one error yeahhh

--- chess/chess.cpp
+++ chess/chess.cpp
@@ -15,41 +15,8 @@ int moves[100][5] = {}; // x, y, new x, new y, peice
 int counter = 0;
 
 
-int main (){
-    ifstream fin ("chess/initial-board.in");
-    srand(time(0));
-    for (int i = 0; i<8; i++){
-        for (int j = 0; j<8; j++){
-            fin >> places[i][j];
-        }
-    }
-    printBoard();
-    moves();
-    createMoves(-1,0,0);
-}
 
-void moves(){
-    for (int i = 0; i<8; i++){
-        for (int j = 0; j<8; j++){
-            switch (places[i][j]){
-                case 0 :
-                    //exit
-                case 1 || -1 :
-                    pawn(i,j);
-                case 2 || -2 :
-                    rook(i,j);
-                case 3 || -3 :
-                    knight(i,j);
-                case 4 || -4 :
-                    bishop(i,j);
-                case 5 || -5 :
-                    king(i,j);
-                case 6 || -6 :
-                    queen(i,j);
-            }
-        }
-    }
-}
+
 int createMoves (int piece, int x, int y){
     int kindOfPeice = abs(piece);
     int side = 0;
@@ -60,16 +27,18 @@ int createMoves (int piece, int x, int y){
     return 0;
 }
 
-void sign (int num){
+int sign (int num){
     int sign = num/abs(num);
     return sign;
 }
 
+//void asignMove (int move[5]){
 
+//}
 
-void rook(int x, int y){
-    for ()
-}
+//void rook(int x, int y){
+    //for ()
+//}
 
 void knight(int x, int y){
 
@@ -81,11 +50,10 @@ void queen(int x, int y){
 
 void pawn(int x, int y){
     if (places[x][y+sign(places[x][y])]==0){
-        moves[counter][0]= x;
-        moves[counter][1]= y;
-        moves[counter][3]= x;
-        moves[counter][4]= y+sign(places[x][y]);
-        moves[counter][5]= sign(places[x][y]);
+        int move[5] = {x, y, x, y+sign(places[x][y]), sign(places[x][y])};
+        //asignMove(move);
+
+        moves[counter] = move;
     }
     counter++;
 }
@@ -107,4 +75,41 @@ void bishop(int x, int y){
          cout << endl;
      }
      return 0;
+ }
+
+
+ void next(){
+     for (int i = 0; i<8; i++){
+         for (int j = 0; j<8; j++){
+             switch (places[i][j]){
+                 case 0 :
+                     //exit
+                 case 1 :
+                     pawn(i,j);
+                 case 2:
+                     //rook(i,j);
+                 case 3:
+                     knight(i,j);
+                 case 4:
+                     bishop(i,j);
+                 case 5:
+                     king(i,j);
+                 case 6:
+                     queen(i,j);
+             }
+         }
+     }
+ }
+
+ int main (){
+     ifstream fin ("chess/initial-board.in");
+     srand(time(0));
+     for (int i = 0; i<8; i++){
+         for (int j = 0; j<8; j++){
+             fin >> places[i][j];
+         }
+     }
+     printBoard();
+     next();
+     createMoves(-1,0,0);
  }
