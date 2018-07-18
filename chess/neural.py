2d8325236e5dadde05f6a1936dfee9e4eb858bab
Thomas Bueler <twbueler@gmail.com>
2018-07-17 21:17:24 -0800

    Restarting

--- chess/neural.py
+++ chess/neural.py
@@ -2,18 +2,27 @@ import math
 import numpy as np
 from random import *
 
+#inputs = [[1,0,0],[1,0,1],[0,0,1]]
+#output = [0,1,1]
+input = np.array([1,0,0])
+w = np.random.random(3) # weights
+b = 0  # bias
+
+
 def sig(x):
     return 1/(1+math.exp(-x))
 
 def dx(x):
     return sig(x)*(1-sig(x))
 
-input = [[1,0,0],[1,0,1],[0,0,1]]
-output = [0,1,1]
-weights = np.zeros(3)
+def optimization():
+    nueron = sig(np.sum(np.multiply(input,w))-b)
+    print nueron
+
+
+
 print input
-print output
-print weights
+print w
+print np.multiply(input,w)
 
-
-print dx(0)
+optimization()
