1f825a3b731b789542136a068f9545bef47829ab
Thomas Bueler <twbueler@gmail.com>
2018-07-19 17:39:55 -0800

    got function to optimize

--- chess/neural.py
+++ chess/neural.py
@@ -3,10 +3,10 @@ import numpy as np
 from random import *
 
 #inputs = [[1,0,0],[1,0,1],[0,0,1]]
-#output = [0,1,1]
-input = np.array([1,0,0])
+y = np.array([0,1,1])
+x = np.array([[1,0,0],[1,0,1],[0,0,1]])
 w = np.random.random(3) # weights
-b = 0  # bias
+b = 0.5  # bias
 
 
 def sig(x):
@@ -16,13 +16,14 @@ def dx(x):
     return sig(x)*(1-sig(x))
 
 def optimization():
-    nueron = sig(np.sum(np.multiply(input,w))-b)
+    x0 = sig(np.sum(np.multiply(x[0],w))-b)
+    x1 = sig(np.sum(np.multiply(x[1],w))-b)
+    x2 = sig(np.sum(np.multiply(x[2],w))-b)
+    nueron = (y[0]-x0)**2+(y[1]-x1)**2+(y[1]-x1)**2
     print nueron
 
-
-
-print input
+print x
 print w
-print np.multiply(input,w)
+#print np.multiply(x,w)
 
 optimization()
